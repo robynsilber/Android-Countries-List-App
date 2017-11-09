a3b36e69bb87f857d80df26e1cc8541368fe7a5e
Robyn Silber <rjsilber@gmail.com>
2017-11-09 10:30:40 -0500

    Implemented CountryDataAsyncTask doInBackground() method has been implemented but contains a bug when connecting to the API

--- app/src/main/java/com/robynsilber/countries_list_app/CountriesActivity.java
+++ app/src/main/java/com/robynsilber/countries_list_app/CountriesActivity.java
@@ -3,11 +3,28 @@ package com.robynsilber.countries_list_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CountriesActivity extends AppCompatActivity {
+public class CountriesActivity extends AppCompatActivity implements CountryDataAsyncTask.IAsyncTaskResponder {
+
+    private Country[] mCountryModel;
+    private CountryDataAsyncTask mCountryDataAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countries);
+        retrieveCountryData();
+    }
+
+    public void retrieveCountryData(){
+
+        mCountryDataAsyncTask = new CountryDataAsyncTask(this); // instantiates CountryDataAsyncTask
+
+        // execute the WeatherDataAsyncTask; pass in the lat and lon
+        mCountryDataAsyncTask.execute();
+    }
+
+    @Override
+    public void asyncTaskFinished(Country[] countries) {
+
     }
 }
--- app/src/main/java/com/robynsilber/countries_list_app/CountryDataAsyncTask.java
+++ app/src/main/java/com/robynsilber/countries_list_app/CountryDataAsyncTask.java
@@ -1,13 +1,114 @@
 package com.robynsilber.countries_list_app;
 
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.util.Log;
 
-public class CountryDataAsyncTask extends AsyncTask<String, Void, Country[]> {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CountryDataAsyncTask extends AsyncTask<Void, Void, Country[]> {
+
+    public interface IAsyncTaskResponder {
+        void asyncTaskFinished(Country[] countries);
+    }
+
+    public IAsyncTaskResponder mIAsyncTaskListener = null;
+
+    public CountryDataAsyncTask(IAsyncTaskResponder responder){
+        this.mIAsyncTaskListener = responder;
+    }
+
+    private final String LOG_TAG = CountryDataAsyncTask.class.getSimpleName();
 
 
     @Override
-    protected Country[] doInBackground(String... strings) {
+    protected Country[] doInBackground(Void... voids) {
+
+        final String BASE_URL = "https://restcountries.eu/rest/v2/all?fields=name";
+
+        String jsonData = null; // stores the retrieved json data
+
+        HttpURLConnection httpURLConnection = null;
+        BufferedReader bufferedReader = null;
+
+        try{
+            Uri uri = Uri.parse(BASE_URL).buildUpon().build();
+
+            // Declare the URL, using the uri String as arg
+            URL url = new URL(uri.toString()); // requires an IOException as the catch block param
+//            URL url = new URL(BASE_URL);
+            // Begin query to server by opening httpURLConnection
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method of the httpURLConnection to "GET"
+            httpURLConnection.setRequestMethod("GET");
+
+
+            // Connect to server
+            httpURLConnection.connect();
+
+            // Next step: Retrieve the country data
+            // Proceed by getting the input stream returned from the query
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            if(inputStream == null){ // Failed to retrieve data
+                Log.d(LOG_TAG, "Failed to retrieve country data");
+                return null; // no data retrieved from server
+            }
+
+            // Parse the retrieved data
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+            // initialize bufferedReader and stringBuffer
+            bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            // Decalare variables for parsing, reading data line by line (using the bufferedReader)
+            String line;
+            final String NEW_LINE = "\n";
+
+            while((line=bufferedReader.readLine()) != null){
+                // append stringBuffer by adding new line escape char.
+                // The following is for readability and debugging/testing purposes
+                stringBuffer.append(line + NEW_LINE);
+            } // all the data has been initially parsed and appended
+
+            if(stringBuffer.length() == 0){ // nothing to parse
+                Log.d(LOG_TAG, "No data to parse");
+                return null;
+            }
+
+            // Successful retrieval of JSON country data
+            jsonData = stringBuffer.toString();
+            Log.d(LOG_TAG, jsonData);
+
+
+        }catch(IOException e){
+            Log.e(LOG_TAG, "Error: ", e);
+            return null;
+        }finally {
+            // Disconnect the httpURLConnection
+            if(httpURLConnection != null){
+                httpURLConnection.disconnect();
+            }
+
+            // Close the buffered reader
+            if(bufferedReader != null){
+                try{
+                    bufferedReader.close();
+                } catch(final IOException e){
+                    Log.e(LOG_TAG, "Error: BufferedReader failed to close: ", e);
+                }
+            }
+        }
+
+
         return new Country[0];
     }
 }
